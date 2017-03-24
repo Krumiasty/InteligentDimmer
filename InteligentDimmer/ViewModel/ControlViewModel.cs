9ed32b998e0ea4e3c4f1f4ca8f2ad426755d6b25
Krum <krruuum@wp.pl>
2017-03-25 00:09:01 +0100

    Bluetooth two-way connection

--- InteligentDimmer/ViewModel/ControlViewModel.cs
+++ InteligentDimmer/ViewModel/ControlViewModel.cs
@@ -1,11 +1,17 @@
 ﻿using System.ComponentModel;
+using System.IO.Ports;
+using System.Net.Mime;
+using System.Windows;
+using System.Windows.Input;
 using InteligentDimmer.Model;
+using InteligentDimmer.Utility;
 
 namespace InteligentDimmer.ViewModel
 {
     public class ControlViewModel: INotifyPropertyChanged
     {
         private Bluetooth _selectedBluetooth;
+        public ICommand WindowCloseCommand { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -18,12 +24,35 @@ namespace InteligentDimmer.ViewModel
                 RaisePropertyChanged("SelectedBluetooth");
             }
         }
+
+        public SerialPort SerialPort { get; }
+
         public ControlViewModel()
         {
-            
+                // TODO check which ctor is being used
         }
 
+        public ControlViewModel(SerialPort serialPort)
+        {
+            SerialPort = serialPort;
+            LoadCommands();
+        }
 
+        private void LoadCommands()
+        {
+            WindowCloseCommand = new CustomCommand(WindowClose, CanClose);
+        }
+
+        private bool CanClose(object obj)
+        {
+            return true;
+        }
+
+        private void WindowClose(object obj)
+        {
+          //  SerialPort.Close();
+            Application.Current.Shutdown();
+        }             
 
         private void RaisePropertyChanged(string propertyName)
         {
@@ -31,8 +60,6 @@ namespace InteligentDimmer.ViewModel
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
-        }
-
-      
+        }     
     }
 }
