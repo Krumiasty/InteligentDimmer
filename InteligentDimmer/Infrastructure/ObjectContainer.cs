9ce78f441dd3dc6812cb11edf731b6e5167c0e95
Krum <krruuum@wp.pl>
2017-04-18 19:46:42 +0200

    ViewModelLocator added

---
+++ InteligentDimmer/Infrastructure/ObjectContainer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InteligentDimmer.Infrastructure
+{
+    public class ObjectContainer
+    {
+        private readonly IList<object> _viewModelContainer;
+
+        private static readonly Lazy<ObjectContainer> _lazyInstance = new Lazy<ObjectContainer>(() => new ObjectContainer(), true);
+
+        public static ObjectContainer Instance => _lazyInstance.Value;
+
+        private ObjectContainer()
+        {
+            _viewModelContainer = new List<object>();
+        }
+
+        public T GetInstance<T>()
+        {
+            var requestedType = _viewModelContainer.FirstOrDefault(x => x.GetType() == typeof(T));
+
+            if (requestedType != null)
+            {
+                return (T)requestedType;
+            }
+
+            T newType = Activator.CreateInstance<T>();
+
+            _viewModelContainer.Add(newType);
+
+            return newType;
+        }
+    }
+}
